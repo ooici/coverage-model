963928b05ee58253b0826c411cae073d05ca2c06
James Case <case@oceanz.org>
2012-10-16 10:09:08 -0400

    Adds filling of bricks.

--- coverage_model/test/perftest.py
+++ coverage_model/test/perftest.py
@@ -13,9 +13,10 @@ from coverage_model.coverage import *
 from coverage_model.parameter_types import *
 import numpy as np
 import time
-import rtree
 
-def _make_cov(brick_size=None, chunk_size=None):
+all_dtypes = ['bool','int','int8','int16','int32','int64','uint8','uint16','uint32','uint64','float','float16','float32','float64','complex','complex64','complex128']
+
+def _make_cov(brick_size=None, chunk_size=None, dtype='int64'):
     bricking_scheme = None
 
     if brick_size and chunk_size is not None:
@@ -32,7 +33,7 @@ def _make_cov(brick_size=None, chunk_size=None):
     pdict = ParameterDictionary()
 
     # Create a set of ParameterContext objects to define the parameters in the coverage, add each to the ParameterDictionary
-    t_ctxt = ParameterContext('quantity_time', param_type=QuantityType(value_encoding=np.dtype('int64')), variability=VariabilityEnum.TEMPORAL)
+    t_ctxt = ParameterContext('quantity_time', param_type=QuantityType(value_encoding=np.dtype(dtype)), variability=VariabilityEnum.TEMPORAL)
     t_ctxt.reference_frame = AxisTypeEnum.TIME
     t_ctxt.uom = 'seconds since 01-01-1970'
     pdict.add_context(t_ctxt)
@@ -42,40 +43,23 @@ def _make_cov(brick_size=None, chunk_size=None):
 
     return scov
 
-def _set_data(scov, size, origin, repeat, delay):
-    timer = []
-    # Expand the domain
-    si=time.time()
-    scov.insert_timesteps(size)
-    sis=time.time()-si
-
-    # Add data for each parameter
-    loc = slice(origin, origin+size)
-
-    for i in xrange(repeat):
-        st = time.time()
-
-        scov.set_parameter_values('quantity_time', value=np.arange(size), tdoa=loc)
-        timer.append(time.time()-st)
-
-        # Sleep for delay (seconds)
-        time.sleep(delay)
-
-    return timer, sis
-
-def _fill_coverage(scov, size, limit):
+def _fill(scov, size, limit):
     insert_timer = []
     expand_timer = []
 
     # Add data for each parameter
     origins = np.arange(limit)[1::size]-1
     for origin in origins:
+
         # Expand the domain
         si=time.time()
         scov.insert_timesteps(size)
         expand_timer.append(time.time()-si)
 
-        loc = slice(origin, origin+size)
+        upper_bnd = origin + size
+        if upper_bnd > limit:
+            upper_bnd = limit
+        loc = slice(origin, upper_bnd)
         st = time.time()
 
         scov.set_parameter_values('quantity_time', value=np.arange(size)+origin, tdoa=loc)
@@ -83,129 +67,147 @@ def _fill_coverage(scov, size, limit):
 
     return insert_timer, expand_timer
 
-def run_perf_test(size=10, origin=0, repeat=1, delay=0, brick_size=None, chunk_size=None):
-    scov = _make_cov(brick_size, chunk_size)
-    timer, insert_time = _set_data(scov, size, origin, repeat, delay)
-    avg = sum(timer)/len(timer)
-    return avg, timer, insert_time
-
-# avg, timer, insert_time = run_perf_fill_test(size=100, limit=10000, repeat=1, delay=0, brick_size=20000, chunk_size=500)
-def run_perf_fill_test(sizes=[10], limits=[300], brick_size=50, chunk_size=25):
-    scov = _make_cov(brick_size, chunk_size)
-
+# run_perf_fill_test(sizes=[1,5,10,15,20,25], limit=300, brick_size=100, chunk_size=25, dtypes=['int64'])
+def run_perf_fill_test(sizes=[100], limit=86400, brick_size=10000, chunk_size=1000, dtypes=all_dtypes):
     t = time.localtime()
     str_time = '{0}{1}{2}{3}{4}'.format(t.tm_year,t.tm_mon,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec)
     output_file = 'perf_result-{0}.csv'.format(str_time)
 
-    header = 'Size,Limit,Brick Size,Chunk Size,Insert Average,Expand Average\n'
+    scov_dict = {}
+
+    header = 'Path,Dtype,Data Write Size,Max Data Points,Brick Size,Chunk Size,Insert Average,Expand Average\n'
     with open(output_file, 'w') as f:
         f.write(header)
 
-        for s in sizes:
-            for l in limits:
-                print 'Working on: {0},{1},{2},{3}'.format(s, l, brick_size, chunk_size)
-                insert_timer, expand_timer = _fill_coverage(scov, s, l)
+        for dtype in dtypes:
+            for size in sizes:
+                print 'Working on: {0},{1},{2},{3},{4}'.format(dtype, size, limit, brick_size, chunk_size)
+                scov = _make_cov(brick_size, chunk_size, dtype=dtype)
+                pl_scov = scov._persistence_layer
+                scov_path = os.path.join(pl_scov.master_manager.root_dir, pl_scov.master_manager.guid)
+                insert_timer, expand_timer = _fill(scov, size, limit)
                 insert_avg = sum(insert_timer)/len(insert_timer)
                 expand_avg = sum(expand_timer)/len(expand_timer)
-                line = '{0},{1},{2},{3},{4},{5},{6},{7}\n'.format(s, l, brick_size, chunk_size, insert_avg, expand_avg, insert_timer, expand_timer)
+                line = '{0},{1},{2},{3},{4},{5},{6},{7}\n'.format(scov_path, dtype, size, limit, brick_size, chunk_size, insert_avg, expand_avg)
                 f.write(line)
+                print 'Folder Size for {0}: {1}'.format(scov_path, size_dir(scov_path))
+                scov_dict[dtype] = scov_path
 
-def wtf():
-    brick_size = 10
-    chunk_size = 5
-    size = 10
+    for s in scov_dict:
+        print 'Folder Size for dtype ({2}) at location {0}: {1}'.format(scov_dict[s], size_dir(scov_dict[s]), s)
 
-    loc = slice(0, size)
+    return scov_dict
 
-    scov = _make_cov(brick_size, chunk_size)
-    scov.insert_timesteps(size)
-    scov.set_parameter_values('quantity_time', value=np.arange(size), tdoa=loc)
-    scov.set_parameter_values('quantity', value=np.random.random_sample(size)*(26-23)+23, tdoa=loc)
+def size_dir(d):
+    import os
+    from os.path import join, getsize
+    s = 0
+    for root, dirs, files in os.walk(d):
+        s = s + sum(getsize(join(root, name)) for name in files)
+    return s
 
-# test_all(max_size=100000, max_repeat=1, max_delay=0, max_brick_size=50000, max_chunk_size=500, size_step=500, fixed_size=None, brick_step=50, fixed_bricking=1)
-def test_all(max_size, max_repeat, max_delay, max_brick_size, max_chunk_size, size_step=5, fixed_size=None, brick_step=5, fixed_bricking=None):
-    t = time.localtime()
-    str_time = '{0}{1}{2}{3}{4}'.format(t.tm_year,t.tm_mon,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec)
-    output_file = 'perf_result-{0}.csv'.format(str_time)
+#def getFolderSize(folder):
+#    total_size = os.path.getsize(folder)
+#    for item in os.listdir(folder):
+#        itempath = os.path.join(folder, item)
+#        if os.path.isfile(itempath):
+#            total_size += os.path.getsize(itempath)
+#        elif os.path.isdir(itempath):
+#            total_size += getFolderSize(itempath)
+#    return total_size
 
-    header = 'Origin,Size,Repeat,Delay,Brick Size,Chunk Size,Average,Raw\n'
-    with open(output_file, 'w') as f:
-        f.write(header)
+#def run_perf_test(size=10, origin=0, repeat=1, delay=0, brick_size=None, chunk_size=None):
+#    scov = _make_cov(brick_size, chunk_size)
+#    timer, insert_time = _set_data(scov, size, origin, repeat, delay)
+#    avg = sum(timer)/len(timer)
+#    return avg, timer, insert_time
+#
+#def _set_data(scov, size, origin, repeat, delay):
+#    timer = []
+#    # Expand the domain
+#    si=time.time()
+#    scov.insert_timesteps(size)
+#    sis=time.time()-si
+#
+#    # Add data for each parameter
+#    loc = slice(origin, origin+size)
+#
+#    for i in xrange(repeat):
+#        st = time.time()
+#
+#        scov.set_parameter_values('quantity_time', value=np.arange(size), tdoa=loc)
+#        timer.append(time.time()-st)
+#
+#        # Sleep for delay (seconds)
+#        time.sleep(delay)
+#
+#    return timer, sis
+#
+## test_all(max_size=100000, max_repeat=1, max_delay=0, max_brick_size=50000, max_chunk_size=500, size_step=500, fixed_size=None, brick_step=50, fixed_bricking=1)
+#def test_all(max_size, max_repeat, max_delay, max_brick_size, max_chunk_size, size_step=5, fixed_size=None, brick_step=5, fixed_bricking=None):
+#    t = time.localtime()
+#    str_time = '{0}{1}{2}{3}{4}'.format(t.tm_year,t.tm_mon,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec)
+#    output_file = 'perf_result-{0}.csv'.format(str_time)
+#
+#    header = 'Origin,Size,Repeat,Delay,Brick Size,Chunk Size,Average,Raw\n'
+#    with open(output_file, 'w') as f:
+#        f.write(header)
+#
+#        repeats = np.arange(max_repeat+1)[1:]
+##        repeats = repeats.tolist()
+#        if max_delay > 0:
+#            delays = np.arange(max_delay+1)[1:]
+##            delays = delays.tolist()
+#        else:
+#            delays = [0]
+#
+#        if fixed_size is None:
+#            sizes = np.arange(max_size+size_step)[1::size_step][1:]-1
+##            sizes = sizes.tolist()
+#        else:
+#            sizes = [max_size]
+#
+#        if fixed_bricking is None:
+#            brick_sizes = np.arange(max_brick_size+brick_step)[1::brick_step][1:]-1
+#            chunk_sizes = np.arange(max_chunk_size+brick_step)[1::brick_step][1:]-1
+#        else:
+#            brick_sizes = [max_brick_size]
+#            chunk_sizes = [max_chunk_size]
+#
+#        for s in sizes:
+#            for r in repeats:
+#                for d in delays:
+#                    for bs in brick_sizes:
+#                        for cs in chunk_sizes:
+#                            if cs < bs:
+#                                print 'Working on: {0},{1},{2},{3},{4},{5}'.format(s, size_step, r, d, bs, cs)
+#                                avg, timer, insert_time = run_perf_test(size=size_step, origin=s, repeat=r, delay=d, brick_size=bs, chunk_size=cs)
+#                                line = '{0},{1},{2},{3},{4},{5},{6},{7},{8}\n'.format(s, size_step, r, d, bs, cs, avg, timer, insert_time)
+#                                f.write(line)
+#
+#def test1(sizes, max_repeat, max_delay, brick_sizes, chunk_sizes):
+#    t = time.localtime()
+#    str_time = '{0}{1}{2}{3}{4}'.format(t.tm_year,t.tm_mon,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec)
+#    output_file = 'perf_result-{0}.csv'.format(str_time)
+#
+#    header = 'Origin,Size,Repeat,Delay,Brick Size,Chunk Size,Average,Raw\n'
+#    with open(output_file, 'w') as f:
+#        f.write(header)
+#
+#        repeats = np.arange(max_repeat+1)[1:]
+#        if max_delay > 0:
+#            delays = np.arange(max_delay+1)[1:]
+#        else:
+#            delays = [0]
+#        origin = 0
+#        for s in sizes:
+#            for r in repeats:
+#                for d in delays:
+#                    for bs in brick_sizes:
+#                        for cs in chunk_sizes:
+#                            if cs < bs:
+#                                print 'Working on: {0},{1},{2},{3},{4}'.format(s, r, d, bs, cs)
+#                                avg, timer, insert_time = run_perf_test(size=s, origin=origin, repeat=r, delay=d, brick_size=bs, chunk_size=cs)
+#                                line = '{0},{1},{2},{3},{4},{5},{6},{7},{8}\n'.format(origin, s, r, d, bs, cs, avg, timer, insert_time)
+#                                f.write(line)
 
-        repeats = np.arange(max_repeat+1)[1:]
-#        repeats = repeats.tolist()
-        if max_delay > 0:
-            delays = np.arange(max_delay+1)[1:]
-#            delays = delays.tolist()
-        else:
-            delays = [0]
-
-        if fixed_size is None:
-            sizes = np.arange(max_size+size_step)[1::size_step][1:]-1
-#            sizes = sizes.tolist()
-        else:
-            sizes = [max_size]
-
-        if fixed_bricking is None:
-            brick_sizes = np.arange(max_brick_size+brick_step)[1::brick_step][1:]-1
-            chunk_sizes = np.arange(max_chunk_size+brick_step)[1::brick_step][1:]-1
-        else:
-            brick_sizes = [max_brick_size]
-            chunk_sizes = [max_chunk_size]
-
-        for s in sizes:
-            for r in repeats:
-                for d in delays:
-                    for bs in brick_sizes:
-                        for cs in chunk_sizes:
-                            if cs < bs:
-                                print 'Working on: {0},{1},{2},{3},{4},{5}'.format(s, size_step, r, d, bs, cs)
-                                avg, timer, insert_time = run_perf_test(size=size_step, origin=s, repeat=r, delay=d, brick_size=bs, chunk_size=cs)
-                                line = '{0},{1},{2},{3},{4},{5},{6},{7},{8}\n'.format(s, size_step, r, d, bs, cs, avg, timer, insert_time)
-                                f.write(line)
-
-def test1(sizes, max_repeat, max_delay, brick_sizes, chunk_sizes):
-    t = time.localtime()
-    str_time = '{0}{1}{2}{3}{4}'.format(t.tm_year,t.tm_mon,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec)
-    output_file = 'perf_result-{0}.csv'.format(str_time)
-
-    header = 'Origin,Size,Repeat,Delay,Brick Size,Chunk Size,Average,Raw\n'
-    with open(output_file, 'w') as f:
-        f.write(header)
-
-        repeats = np.arange(max_repeat+1)[1:]
-        if max_delay > 0:
-            delays = np.arange(max_delay+1)[1:]
-        else:
-            delays = [0]
-        origin = 0
-        for s in sizes:
-            for r in repeats:
-                for d in delays:
-                    for bs in brick_sizes:
-                        for cs in chunk_sizes:
-                            if cs < bs:
-                                print 'Working on: {0},{1},{2},{3},{4}'.format(s, r, d, bs, cs)
-                                avg, timer, insert_time = run_perf_test(size=s, origin=origin, repeat=r, delay=d, brick_size=bs, chunk_size=cs)
-                                line = '{0},{1},{2},{3},{4},{5},{6},{7},{8}\n'.format(origin, s, r, d, bs, cs, avg, timer, insert_time)
-                                f.write(line)
-
-
-def rt():
-    p = rtree.index.Property()
-    p.dimension = 2
-    brick_tree = rtree.index.Index(properties=p)
-    brick_tree.insert(0,(0,0,500,0),obj='GUID1')
-    start = [0,0]
-    stop = [500,0]
-    st1 = time.time()
-    hits = list(brick_tree.intersection(tuple(start+stop), objects=True))
-    stp1 = time.time() - st1
-
-    brick_tree.insert(1,(500,0,1000,0),obj='GUID2')
-    start = [500,0]
-    stop = [1000,0]
-    st2 = time.time()
-    hits = list(brick_tree.intersection(tuple(start+stop), objects=True))
-    stp2 = time.time() - st2
-    return stp1, stp2
