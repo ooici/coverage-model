69e924dc920bc5b8dd471b0fd5ec711d319d6497
Christopher Mueller <cmueller@asascience.com>
2012-10-22 15:18:59 -0400

    Fixes issue with single value assignment
    Addresses a problem with assignment of a single value to an array or ConstantType

--- coverage_model/parameter_values.py
+++ coverage_model/parameter_values.py
@@ -180,7 +180,7 @@ class ConstantValue(AbstractComplexParameterValue):
 
     @property
     def content(self):
-        return self._storage[0][()] # Returned from storage as a 0-d array
+        return self._storage[0]
 
     def expand_content(self, domain, origin, expansion):
         # No op storage is always 1 - appropriate domain applied during retrieval of data
