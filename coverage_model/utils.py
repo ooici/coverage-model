380b8d82a527c87707c5b1e1406d1a20dc3efd27
Christopher Mueller <cmueller@asascience.com>
2012-10-23 12:06:21 -0400

    Adds IndexError support when slicing
    Adds functions for catching inappropriate indexing and raises IndexError as appropriate.

--- coverage_model/utils.py
+++ coverage_model/utils.py
@@ -29,6 +29,31 @@ def is_valid_constraint(v):
 
     return ret
 
+def _raise_index_error_int(slice_, shape, dim):
+    if slice_ < 0:
+        raise IndexError('index cannot be < 0: index => {0}'.format(slice_))
+    if slice_ >= shape:
+        raise IndexError('index cannot be >= the size for dimension {2}: index => {0}, size => {1}'.format(slice_, shape, dim))
+
+def _raise_index_error_list(slice_, shape, dim):
+    last = -1
+    for i in xrange(len(slice_)):
+        c=slice_[i]
+        if last >= c:
+            raise IndexError('indices must be in increasing order and cannot be duplicated: list => {0}'.format(slice_))
+        last = c
+        if slice_[i] >= shape:
+            raise IndexError('index {0} of list cannot be >= the size for dimension {3}: list => {1}, size => {2}'.format(i, slice_, shape, dim))
+
+def _raise_index_error_slice(slice_, shape, dim):
+    if slice_.start is not None or slice_.stop is not None:
+        if slice_.start >= slice_.stop:
+            raise IndexError('start index of slice cannot be >= stop index: slice => {0}'.format(slice_))
+        if slice_.start >= shape:
+            raise IndexError('start index of slice cannot be >= size for dimension {2}: slice => {0}, size => {1}'.format(slice_, shape, dim))
+        elif slice_.stop > shape:
+            raise IndexError('stop index of slice cannot be > size for dimension {2}: slice => {0}, size => {1}'.format(slice_, shape, dim))
+
 def fix_slice(slice_, shape):
     # CBM: First swack - see this for more possible checks: http://code.google.com/p/netcdf4-python/source/browse/trunk/netCDF4_utils.py
     if not is_valid_constraint(slice_):
@@ -50,17 +75,22 @@ def fix_slice(slice_, shape):
             for n in xrange(slen, rank):
                 slice_.append(slice(None,None,None))
 
-    # Next, deal with negative indices
+    # Next, deal with negative indices and check for IndexErrors
     # Logic for handling negative indices from: http://docs.scipy.org/doc/numpy/reference/arrays.indexing.html
     for x in xrange(rank):
         sl=slice_[x]
         if isinstance(sl, int):
             if sl < 0:
-                slice_[x] = shape[x] + slice_[x]
+                sl = shape[x] + slice_[x]
+
+            _raise_index_error_int(sl, shape[x], x)
+            slice_[x] = sl
         elif isinstance(sl, list):
             for i in xrange(len(sl)):
                 if sl[i] < 0:
                     sl[i] = shape[x] + sl[i]
+
+            _raise_index_error_list(sl, shape[x], x)
             slice_[x] = sl
         elif isinstance(sl, slice):
             sl_ = [sl.start, sl.stop, sl.step]
@@ -74,15 +104,17 @@ def fix_slice(slice_, shape):
                 if sl_[2] < 0:
                     sl_[0], sl_[1], sl_[2] = sl_[1], sl_[0], -sl_[2]
 
-                # Fix the stop value based on the step
-                s=sl_[0] if sl_[0] is not None else 0
-                e=sl_[1] if sl_[1] is not None and sl_[1] <= shape[x] else shape[x]
-                m=xrange(s, e, sl_[2])[-1]
-                m += 1 # Stop is non-inclusive
-                sl_[1] = m
+#                # Fix the stop value based on the step
+#                s=sl_[0] if sl_[0] is not None else 0
+#                e=sl_[1] if sl_[1] is not None and sl_[1] <= shape[x] else shape[x]
+#                m=xrange(s, e, sl_[2])[-1]
+#                m += 1 # Stop is non-inclusive
+#                sl_[1] = m
 
             # Reassign the slice
             slice_[x] = slice(*sl_)
+
+            _raise_index_error_slice(slice_[x], shape[x], x)
 
 
 
