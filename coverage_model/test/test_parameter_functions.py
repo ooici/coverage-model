f77f3f936236ed203594eb6bea38ffef7287cd53
Christopher Mueller <cmueller@asascience.com>
2013-02-18 17:00:13 -0500

    Adds some tests for ParameterFunctionValidator

--- coverage_model/test/test_parameter_functions.py
+++ coverage_model/test/test_parameter_functions.py
@@ -85,6 +85,7 @@ class TestParameterFunctionsInt(TestCase):
 
     def setUp(self):
         self.contexts = None
+        self.value_classes = None
 
     def _get_param_vals(self, name, slice_):
         shp = utils.slice_shape(slice_, (10,))
@@ -111,14 +112,6 @@ class TestParameterFunctionsInt(TestCase):
             return np.zeros(shp)
 
         return ret
-
-    def _get_pc_dict(self, *pnames):
-        all_pc = self._create_all_params()
-        return {x: all_pc[x] for x in pnames}
-
-    def _get_pc_list(self, *pnames):
-        all_pc = self._create_all_params()
-        return [all_pc[x] for x in pnames]
 
     def _ctxt_callback(self, context_name):
         return self.contexts[context_name]
@@ -205,111 +198,187 @@ class TestParameterFunctionsInt(TestCase):
         rho = gsw.rho(abs_sal, cons_temp, p1val[:])
         self.assertTrue(np.allclose(rhoval[:], rho))
 
+import networkx as nx
+@attr('INT',group='cov')
+class TestParameterValidatorInt(TestCase):
 
-    def _create_all_params(self):
-        '''
-         [
-         'DENSITY',
-         'TIME',
-         'LON',
-         'TEMPWAT_L1',
-         'TEMPWAT_L0',
-         'CONDWAT_L1',
-         'CONDWAT_L0',
-         'PRESWAT_L1',
-         'PRESWAT_L0',
-         'LAT',
-         'PRACSAL'
-         ]
-        @return:
-        '''
+    def setUp(self):
+        pass
 
-        contexts = {}
+    def test_simple_passthrough(self):
 
-        t_ctxt = ParameterContext('TIME', param_type=QuantityType(value_encoding=np.dtype('int64')))
-        t_ctxt.uom = 'seconds since 01-01-1900'
-        contexts['TIME'] = t_ctxt
+        in_values = _get_pc_list('TIME', 'LAT', 'LON', 'TEMPWAT_L0', 'CONDWAT_L0', 'PRESWAT_L0')
+        in_contexts = in_values
+        out_contexts = _get_pc_list('TIME', 'LAT', 'LON', 'TEMPWAT_L0', 'CONDWAT_L0', 'PRESWAT_L0')
+        out_values = [p.name for p in out_contexts]
 
-        lat_ctxt = ParameterContext('LAT', param_type=ConstantType(QuantityType(value_encoding=np.dtype('float32'))), fill_value=-9999)
-        lat_ctxt.axis = AxisTypeEnum.LAT
-        lat_ctxt.uom = 'degree_north'
-        contexts['LAT'] = lat_ctxt
+        pfv = ParameterFunctionValidator(in_values, in_contexts, out_contexts)
 
-        lon_ctxt = ParameterContext('LON', param_type=ConstantType(QuantityType(value_encoding=np.dtype('float32'))), fill_value=-9999)
-        lon_ctxt.axis = AxisTypeEnum.LON
-        lon_ctxt.uom = 'degree_east'
-        contexts['LON'] = lon_ctxt
+        for o in out_values:
+            g = pfv.validate(o)
+            self.assertIsInstance(g, nx.DiGraph)
 
-        # Independent Parameters
+    def test_L1_from_L0(self):
+        in_values = _get_pc_list('TIME', 'LAT', 'LON', 'TEMPWAT_L0', 'CONDWAT_L0', 'PRESWAT_L0')
+        in_contexts = in_values
+        out_contexts = _get_pc_list('TEMPWAT_L1', 'CONDWAT_L1', 'PRESWAT_L1')
+        out_values = [p.name for p in out_contexts]
 
-        # Temperature - values expected to be the decimal results of conversion from hex
-        temp_ctxt = ParameterContext('TEMPWAT_L0', param_type=QuantityType(value_encoding=np.dtype('float32')), fill_value=-9999)
-        temp_ctxt.uom = 'deg_C'
-        contexts['TEMPWAT_L0'] = temp_ctxt
+        pfv = ParameterFunctionValidator(in_values, in_contexts, out_contexts)
 
-        # Conductivity - values expected to be the decimal results of conversion from hex
-        cond_ctxt = ParameterContext('CONDWAT_L0', param_type=QuantityType(value_encoding=np.dtype('float32')), fill_value=-9999)
-        cond_ctxt.uom = 'S m-1'
-        contexts['CONDWAT_L0'] = cond_ctxt
+        for o in out_values:
+            g = pfv.validate(o)
+            self.assertIsInstance(g, nx.DiGraph)
 
-        # Pressure - values expected to be the decimal results of conversion from hex
-        press_ctxt = ParameterContext('PRESWAT_L0', param_type=QuantityType(value_encoding=np.dtype('float32')), fill_value=-9999)
-        press_ctxt.uom = 'dbar'
-        contexts['PRESWAT_L0'] = press_ctxt
+    def test_L2_from_L0(self):
+        in_values = _get_pc_list('TIME', 'LAT', 'LON', 'TEMPWAT_L0', 'CONDWAT_L0', 'PRESWAT_L0')
+        in_contexts = in_values
+        out_contexts = _get_pc_list('DENSITY', 'PRACSAL', 'TEMPWAT_L1', 'CONDWAT_L1', 'PRESWAT_L1')
+        out_values = ['DENSITY', 'PRACSAL']
+
+        pfv = ParameterFunctionValidator(in_values, in_contexts, out_contexts)
+
+        for o in out_values:
+            g = pfv.validate(o)
+            self.assertIsInstance(g, nx.DiGraph)
+
+    def test_Ln_from_L0(self):
+        in_values = _get_pc_list('TIME', 'LAT', 'LON', 'TEMPWAT_L0', 'CONDWAT_L0', 'PRESWAT_L0')
+        in_contexts = in_values
+        out_contexts = _get_pc_list('DENSITY', 'PRACSAL', 'TEMPWAT_L1', 'CONDWAT_L1', 'PRESWAT_L1')
+        out_values = [p.name for p in out_contexts]
+
+        pfv = ParameterFunctionValidator(in_values, in_contexts, out_contexts)
+
+        for o in out_values:
+            g = pfv.validate(o)
+            self.assertIsInstance(g, nx.DiGraph)
+
+    def test_L2_from_L1(self):
+        in_values = _get_pc_list('TIME', 'LAT', 'LON', 'TEMPWAT_L1', 'CONDWAT_L1', 'PRESWAT_L1')
+        in_contexts = in_values
+        out_contexts = _get_pc_list('DENSITY', 'PRACSAL')
+        out_values = [p.name for p in out_contexts]
+
+        pfv = ParameterFunctionValidator(in_values, in_contexts, out_contexts)
+
+        for o in out_values:
+            g = pfv.validate(o)
+            self.assertIsInstance(g, nx.DiGraph)
 
 
-        # Dependent Parameters
+def _get_pc_dict(*pnames):
+    all_pc = _create_all_params()
+    return {x: all_pc[x] for x in pnames}
 
-        # TEMPWAT_L1 = (TEMPWAT_L0 / 10000) - 10
-        tl1_func = '(T_L0 / 10000) - 10'
-        tl1_pmap = {'T_L0':'TEMPWAT_L0'}
-        expr = NumexprFunction('TEMPWAT_L1', tl1_func, tl1_pmap)
-        tempL1_ctxt = ParameterContext('TEMPWAT_L1', param_type=ParameterFunctionType(function=expr), variability=VariabilityEnum.TEMPORAL)
-        tempL1_ctxt.uom = 'deg_C'
-        contexts['TEMPWAT_L1'] = tempL1_ctxt
+def _get_pc_list(*pnames):
+    all_pc = _create_all_params()
+    return [all_pc[x] for x in pnames]
 
-        # CONDWAT_L1 = (CONDWAT_L0 / 100000) - 0.5
-        cl1_func = '(C_L0 / 100000) - 0.5'
-        cl1_pmap = {'C_L0':'CONDWAT_L0'}
-        expr = NumexprFunction('CONDWAT_L1', cl1_func, cl1_pmap)
-        condL1_ctxt = ParameterContext('CONDWAT_L1', param_type=ParameterFunctionType(function=expr), variability=VariabilityEnum.TEMPORAL)
-        condL1_ctxt.uom = 'S m-1'
-        contexts['CONDWAT_L1'] = condL1_ctxt
+def _create_all_params():
+    '''
+     [
+     'DENSITY',
+     'TIME',
+     'LON',
+     'TEMPWAT_L1',
+     'TEMPWAT_L0',
+     'CONDWAT_L1',
+     'CONDWAT_L0',
+     'PRESWAT_L1',
+     'PRESWAT_L0',
+     'LAT',
+     'PRACSAL'
+     ]
+    @return:
+    '''
 
-        # Equation uses p_range, which is a calibration coefficient - Fixing to 679.34040721
-        #   PRESWAT_L1 = (PRESWAT_L0 * p_range / (0.85 * 65536)) - (0.05 * p_range)
-        pl1_func = '(P_L0 * 679.34040721 / (0.85 * 65536)) - (0.05 * 679.34040721)'
-        pl1_pmap = {'P_L0':'PRESWAT_L0'}
-        expr = NumexprFunction('PRESWAT_L1', pl1_func, pl1_pmap)
-        presL1_ctxt = ParameterContext('PRESWAT_L1', param_type=ParameterFunctionType(function=expr), variability=VariabilityEnum.TEMPORAL)
-        presL1_ctxt.uom = 'S m-1'
-        contexts['PRESWAT_L1'] = presL1_ctxt
+    contexts = {}
 
-        # Density & practical salinity calucluated using the Gibbs Seawater library - available via python-gsw project:
-        #       https://code.google.com/p/python-gsw/ & http://pypi.python.org/pypi/gsw/3.0.1
+    t_ctxt = ParameterContext('TIME', param_type=QuantityType(value_encoding=np.dtype('int64')))
+    t_ctxt.uom = 'seconds since 01-01-1900'
+    contexts['TIME'] = t_ctxt
 
-        # PRACSAL = gsw.SP_from_C((CONDWAT_L1 * 10), TEMPWAT_L1, PRESWAT_L1)
-        owner = 'gsw'
-        sal_func = 'SP_from_C'
-        sal_arglist = [NumexprFunction('CONDWAT_L1*10', 'C*10', {'C':'CONDWAT_L1'}), 'TEMPWAT_L1', 'PRESWAT_L1']
-        sal_kwargmap = None
-        expr = PythonFunction('PRACSAL', owner, sal_func, sal_arglist, sal_kwargmap)
-        sal_ctxt = ParameterContext('PRACSAL', param_type=ParameterFunctionType(expr), variability=VariabilityEnum.TEMPORAL)
-        sal_ctxt.uom = 'g kg-1'
-        contexts['PRACSAL'] = sal_ctxt
+    lat_ctxt = ParameterContext('LAT', param_type=ConstantType(QuantityType(value_encoding=np.dtype('float32'))), fill_value=-9999)
+    lat_ctxt.axis = AxisTypeEnum.LAT
+    lat_ctxt.uom = 'degree_north'
+    contexts['LAT'] = lat_ctxt
 
-        # absolute_salinity = gsw.SA_from_SP(PRACSAL, PRESWAT_L1, longitude, latitude)
-        # conservative_temperature = gsw.CT_from_t(absolute_salinity, TEMPWAT_L1, PRESWAT_L1)
-        # DENSITY = gsw.rho(absolute_salinity, conservative_temperature, PRESWAT_L1)
-        owner = 'gsw'
-        abs_sal_expr = PythonFunction('abs_sal', owner, 'SA_from_SP', ['PRACSAL', 'PRESWAT_L1', 'LON','LAT'], None)
-        cons_temp_expr = PythonFunction('cons_temp', owner, 'CT_from_t', [abs_sal_expr, 'TEMPWAT_L1', 'PRESWAT_L1'], None)
-        dens_expr = PythonFunction('DENSITY', owner, 'rho', [abs_sal_expr, cons_temp_expr, 'PRESWAT_L1'], None)
-        dens_ctxt = ParameterContext('DENSITY', param_type=ParameterFunctionType(dens_expr), variability=VariabilityEnum.TEMPORAL)
-        dens_ctxt.uom = 'kg m-3'
-        contexts['DENSITY'] = dens_ctxt
+    lon_ctxt = ParameterContext('LON', param_type=ConstantType(QuantityType(value_encoding=np.dtype('float32'))), fill_value=-9999)
+    lon_ctxt.axis = AxisTypeEnum.LON
+    lon_ctxt.uom = 'degree_east'
+    contexts['LON'] = lon_ctxt
 
-        return contexts
+    # Independent Parameters
+
+    # Temperature - values expected to be the decimal results of conversion from hex
+    temp_ctxt = ParameterContext('TEMPWAT_L0', param_type=QuantityType(value_encoding=np.dtype('float32')), fill_value=-9999)
+    temp_ctxt.uom = 'deg_C'
+    contexts['TEMPWAT_L0'] = temp_ctxt
+
+    # Conductivity - values expected to be the decimal results of conversion from hex
+    cond_ctxt = ParameterContext('CONDWAT_L0', param_type=QuantityType(value_encoding=np.dtype('float32')), fill_value=-9999)
+    cond_ctxt.uom = 'S m-1'
+    contexts['CONDWAT_L0'] = cond_ctxt
+
+    # Pressure - values expected to be the decimal results of conversion from hex
+    press_ctxt = ParameterContext('PRESWAT_L0', param_type=QuantityType(value_encoding=np.dtype('float32')), fill_value=-9999)
+    press_ctxt.uom = 'dbar'
+    contexts['PRESWAT_L0'] = press_ctxt
+
+
+    # Dependent Parameters
+
+    # TEMPWAT_L1 = (TEMPWAT_L0 / 10000) - 10
+    tl1_func = '(T_L0 / 10000) - 10'
+    tl1_pmap = {'T_L0':'TEMPWAT_L0'}
+    expr = NumexprFunction('TEMPWAT_L1', tl1_func, tl1_pmap)
+    tempL1_ctxt = ParameterContext('TEMPWAT_L1', param_type=ParameterFunctionType(function=expr), variability=VariabilityEnum.TEMPORAL)
+    tempL1_ctxt.uom = 'deg_C'
+    contexts['TEMPWAT_L1'] = tempL1_ctxt
+
+    # CONDWAT_L1 = (CONDWAT_L0 / 100000) - 0.5
+    cl1_func = '(C_L0 / 100000) - 0.5'
+    cl1_pmap = {'C_L0':'CONDWAT_L0'}
+    expr = NumexprFunction('CONDWAT_L1', cl1_func, cl1_pmap)
+    condL1_ctxt = ParameterContext('CONDWAT_L1', param_type=ParameterFunctionType(function=expr), variability=VariabilityEnum.TEMPORAL)
+    condL1_ctxt.uom = 'S m-1'
+    contexts['CONDWAT_L1'] = condL1_ctxt
+
+    # Equation uses p_range, which is a calibration coefficient - Fixing to 679.34040721
+    #   PRESWAT_L1 = (PRESWAT_L0 * p_range / (0.85 * 65536)) - (0.05 * p_range)
+    pl1_func = '(P_L0 * 679.34040721 / (0.85 * 65536)) - (0.05 * 679.34040721)'
+    pl1_pmap = {'P_L0':'PRESWAT_L0'}
+    expr = NumexprFunction('PRESWAT_L1', pl1_func, pl1_pmap)
+    presL1_ctxt = ParameterContext('PRESWAT_L1', param_type=ParameterFunctionType(function=expr), variability=VariabilityEnum.TEMPORAL)
+    presL1_ctxt.uom = 'S m-1'
+    contexts['PRESWAT_L1'] = presL1_ctxt
+
+    # Density & practical salinity calucluated using the Gibbs Seawater library - available via python-gsw project:
+    #       https://code.google.com/p/python-gsw/ & http://pypi.python.org/pypi/gsw/3.0.1
+
+    # PRACSAL = gsw.SP_from_C((CONDWAT_L1 * 10), TEMPWAT_L1, PRESWAT_L1)
+    owner = 'gsw'
+    sal_func = 'SP_from_C'
+    sal_arglist = [NumexprFunction('CONDWAT_L1*10', 'C*10', {'C':'CONDWAT_L1'}), 'TEMPWAT_L1', 'PRESWAT_L1']
+    sal_kwargmap = None
+    expr = PythonFunction('PRACSAL', owner, sal_func, sal_arglist, sal_kwargmap)
+    sal_ctxt = ParameterContext('PRACSAL', param_type=ParameterFunctionType(expr), variability=VariabilityEnum.TEMPORAL)
+    sal_ctxt.uom = 'g kg-1'
+    contexts['PRACSAL'] = sal_ctxt
+
+    # absolute_salinity = gsw.SA_from_SP(PRACSAL, PRESWAT_L1, longitude, latitude)
+    # conservative_temperature = gsw.CT_from_t(absolute_salinity, TEMPWAT_L1, PRESWAT_L1)
+    # DENSITY = gsw.rho(absolute_salinity, conservative_temperature, PRESWAT_L1)
+    owner = 'gsw'
+    abs_sal_expr = PythonFunction('abs_sal', owner, 'SA_from_SP', ['PRACSAL', 'PRESWAT_L1', 'LON','LAT'], None)
+    cons_temp_expr = PythonFunction('cons_temp', owner, 'CT_from_t', [abs_sal_expr, 'TEMPWAT_L1', 'PRESWAT_L1'], None)
+    dens_expr = PythonFunction('DENSITY', owner, 'rho', [abs_sal_expr, cons_temp_expr, 'PRESWAT_L1'], None)
+    dens_ctxt = ParameterContext('DENSITY', param_type=ParameterFunctionType(dens_expr), variability=VariabilityEnum.TEMPORAL)
+    dens_ctxt.uom = 'kg m-3'
+    contexts['DENSITY'] = dens_ctxt
+
+    return contexts
 
 
 
